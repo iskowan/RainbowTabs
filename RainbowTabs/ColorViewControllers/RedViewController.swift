022ac376b753734c643b1ed664d767a086d32393
Anderson <112644138+iskowan@users.noreply.github.com>
2023-04-21 14:09:54 -0400

    Commit 1

--- RainbowTabs/ColorViewControllers/RedViewController.swift
+++ RainbowTabs/ColorViewControllers/RedViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tabBarItem.badgeValue = "!"
         // Do any additional setup after loading the view.
     }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
 
+        tabBarItem.badgeValue = nil
+    }
 
 }
 
